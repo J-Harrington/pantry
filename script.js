2bb889757e4ee9154a903b94cf6bf51d300bfff1
James Harrington <jamesharrington585@hotmail.com>
2024-01-16 00:09:22 -0500

    input value now appends as a list item

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let ingredientSubmit = document.getElementById("ingr-submit");
 let ingredientInput = document.getElementById("ingr-input");
+let ingredientStack = document.getElementById("ingr-stack");
 
 ingredientSubmit.addEventListener("click", function (event) {
   event.preventDefault();
 
   let ingredient = ingredientInput.value;
   console.log(ingredient);
+
+  let ingredientLi = document.createElement("li");
+  ingredientLi.textContent = ingredient;
+  ingredientStack.appendChild(ingredientLi);
 });
